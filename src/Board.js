f3d7d6f0351b8f4e6cd27789baac879958ce6653
matteo <yakumat@gmail.com>
2022-02-14 19:37:38 +0100

    implementazione gioco (in itinere)

--- src/Board.js
+++ src/Board.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Square from "./Square";
 
 function Board() {
-  const renderSquare = (i) => <Square />;
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  const handleClick = (i) => {
+    const newSquares = [...squares];
+    newSquares[i] = "X";
+    setSquares(newSquares);
+  };
+
+  const renderSquare = (i) => (
+    <Square value={squares[i]} onClick={() => handleClick(i)} />
+  );
 
   const status = "Next player: X";
 
